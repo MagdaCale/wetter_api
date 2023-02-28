22ed6aab1a1a12fabca419c64cb451537a3ce54a
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2023-02-28 19:46:42 +0100

    commit

---
+++ src/components/GeoButton.jsx
@@ -0,0 +1,6 @@
+const GeoButton = (props) => {
+    return(
+        <button onClick={() => props.setGeo([props.long, props.lang])}>{props.ort}</button>
+    )
+}
+export default GeoButton
---
+++ src/components/WetterData.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+import GeoButton from "./GeoButton"
+
+const WetterData = () => {
+    const [geo, setGeo] = useState([0,0])
+    const [data, setData]= useState([])
+    
+    // WetterData.jsx:8 Uncaught TypeError: Cannot read properties of undefined (reading '0')
+    /* let weatherIconCode = data.weather.[0].icon   */
+
+
+    // prüfen, ob data.weather definiert ist, bevor auf seine Eigenschaften zugegriffen wird. Dadurch wird weatherIconCode auf undefiniert gesetzt, wenn entweder data.weather oder data.weather[0] undefiniert ist, wodurch der Fehler vermieden wird.
+    let weatherIconCode = data.weather?.[0]?.icon  
+    
+    useEffect(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geo[0]}&lon=${geo[1]}&appid=ab2195502cd0245484dec00c47137016&units=Metric`)
+        .then(res => res.json())
+        .then(data => setData(data))
+        
+    },[geo])
+
+    return(
+        <section>
+            <article>
+                <GeoButton ort='Hamburg' long='53.5536424' lang='9.9595822' setGeo={setGeo} />
+                <GeoButton ort='Berlin' long='52.5195422' lang='13.3981549' setGeo={setGeo}/>
+                <GeoButton ort='Köln' long='50.9578353' lang='6.8272402' setGeo={setGeo}/>
+                <GeoButton ort='Australien' long='-33.847927' lang='150.6517938' setGeo={setGeo}/>
+            </article>
+
+            <article>{data.weather && // react conditional rendering // inline if
+                <p>{data.weather[0].description} in {data.name} </p>    
+            }</article>
+            <img src={`http://openweathermap.org/img/wn/${weatherIconCode}@2x.png`} alt="current weather" /> 
+            <article><p>Current: {data?.main?.temp}</p></article>
+            <article><p>Wind Speed: {data?.wind?.speed} m/std</p></article>
+
+        </section>
+    )
+}
+
+export default WetterData
